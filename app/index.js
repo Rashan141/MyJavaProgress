40664ef6753184edf63afa788305381eb757e453
Rashan141 <rashan141@yahoo.com>
2017-05-24 22:19:28 -0500

    Delete index.js

--- app/index.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var routes = require('./config/routes');
-var Timer = require('react-timer');
-
-ReactDOM.render(
-	<Timer />,
-	document.getElementById('app')
-);
